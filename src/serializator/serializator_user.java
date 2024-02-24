09454dac7ef8baa141a46ccc9e2fc05e1fd14d2b
Dangelcrack <angelguerrero540@gmail.com>
2024-02-24 19:57:18 +0100

    SerializadorIncompleto

--- src/serializator/serializator_user.java
+++ src/serializator/serializator_user.java
@@ -1,4 +1,27 @@
 package serializator;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class serializator_user {
+    String name;
+    String nameUser;
+    String passwordUser;
+    String email;
+
+    public serializator_user(String name, String password, String email, String nameUser) throws FileNotFoundException {
+        try {
+            FileWriter writer = new FileWriter("DatosUsuario.txt", true);
+            this.nameUser = nameUser;
+            this.name = name;
+            this.email = email;
+            this.passwordUser=password;
+            writer.write(name + "\n" + nameUser +"\n");
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
