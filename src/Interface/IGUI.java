0f8a35dc89eec5abf8a95edba9051cb122c4f253
MariaPas235 <mariapm20030523@gmail.com>
2024-02-29 11:27:54 +0100

    crear interfaz view

--- src/Interface/IGUI.java
+++ src/Interface/IGUI.java
@@ -1,4 +1,14 @@
 package Interface;
 
 public interface IGUI {
+void imprimirBienvenida();
+int imprimirMenuInicio();
+int leeNumero(String msg);
+String leeString(String msg);
+
+String recogeDatosInicio();
+
+String recogeDatosRegistro();
+
+
 }
