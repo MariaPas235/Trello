cf5a39a6c849393532df05b4fc299860885610c3
Juan <jsercas2101@g.educaand.es>
2024-02-29 12:57:02 +0100

    imprimir Menu

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,12 +1,25 @@
 package view;
 
+import Interface.IGUI;
+
 import java.util.Scanner;
 
-public class GUI {
+public class GUI implements IGUI {
     Scanner teclado = new Scanner(System.in);
 
     public void imprimirBienvenida() {
-        System.out.println("Bienvenido a nuestro proyecto");
+        System.out.println("________                                                                  ___                                 __________                  ___ ___           \n" +
+                "`MMMMMMMb. 68b                                                   68b      `MM                                 MMMMMMMMMM                  `MM `MM           \n" +
+                " MM    `Mb Y89                                                   Y89       MM                                 /   MM   \\                   MM  MM           \n" +
+                " MM     MM ___   ____   ___  __   ____    ___   ____   ___  __   ___   ____MM   _____            ___              MM     ___  __   ____    MM  MM   _____   \n" +
+                " MM    .M9 `MM  6MMMMb  `MM 6MMb  `MM(    )M'  6MMMMb  `MM 6MMb  `MM  6MMMMMM  6MMMMMb         6MMMMb             MM     `MM 6MM  6MMMMb   MM  MM  6MMMMMb  \n" +
+                " MMMMMMM(   MM 6M'  `Mb  MMM9 `Mb  `Mb    d'  6M'  `Mb  MMM9 `Mb  MM 6M'  `MM 6M'   `Mb       8M'  `Mb            MM      MM69 \" 6M'  `Mb  MM  MM 6M'   `Mb \n" +
+                " MM    `Mb  MM MM    MM  MM'   MM   YM.  ,P   MM    MM  MM'   MM  MM MM    MM MM     MM           ,oMM            MM      MM'    MM    MM  MM  MM MM     MM \n" +
+                " MM     MM  MM MMMMMMMM  MM    MM    MM  M    MMMMMMMM  MM    MM  MM MM    MM MM     MM       ,6MM9'MM            MM      MM     MMMMMMMM  MM  MM MM     MM \n" +
+                " MM     MM  MM MM        MM    MM    `Mbd'    MM        MM    MM  MM MM    MM MM     MM       MM'   MM            MM      MM     MM        MM  MM MM     MM \n" +
+                " MM    .M9  MM YM    d9  MM    MM     YMP     YM    d9  MM    MM  MM YM.  ,MM YM.   ,M9       MM.  ,MM            MM      MM     YM    d9  MM  MM YM.   ,M9 \n" +
+                "_MMMMMMM9' _MM_ YMMMM9  _MM_  _MM_     M       YMMMM9  _MM_  _MM__MM_ YMMMMMM_ YMMMMM9        `YMMM9'Yb.         _MM_    _MM_     YMMMM9  _MM__MM_ YMMMMM9  \n" +
+                "                                                                                                                                                            ");
     }
 
     public String leeString(String msg) {
@@ -21,6 +34,16 @@ public class GUI {
         } while (entrada.isEmpty());
 
         return entrada;
+    }
+
+    @Override
+    public String recogeDatosInicio() {
+        return null;
+    }
+
+    @Override
+    public String recogeDatosRegistro() {
+        return null;
     }
 
     public int imprimirMenuInicio() {
