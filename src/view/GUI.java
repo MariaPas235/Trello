7db9c8188532a639fc6f49acfc330a646607cd70
Juan <jsercas2101@g.educaand.es>
2024-02-29 12:09:20 +0100

    View inicio

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,4 +1,45 @@
 package view;
 
+import java.util.Scanner;
+
 public class GUI {
+    Scanner teclado = new Scanner(System.in);
+    public String leeString(String msg) {
+        String entrada = "";
+        do {
+            System.out.print(msg + ": ");
+            try {
+                entrada = teclado.nextLine();
+            } catch (Exception e) {
+                teclado.next();
+            }
+        } while (entrada.isEmpty());
+
+        return entrada;
+    }
+
+    public int mostrarMenuInicio() {
+        System.out.println("1. Registrarse");
+        System.out.println("2. Iniciar sesión");
+        int opcion = leeNumero("Inserte una opción");
+        return opcion;
+    }
+
+    public int leeNumero(String msg) {
+        boolean aux = false;
+        int numero = 0;
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.print(msg + ": ");
+            try {
+                numero = teclado.nextInt();
+                aux = true;
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Entrada inválida. Introduce un número válido.");
+                teclado.next();
+            }
+        } while (!aux);
+
+        return numero;
+    }
 }
