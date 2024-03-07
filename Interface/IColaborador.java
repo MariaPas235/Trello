cf76945910d026899aa927802b882b0b471b6566
Manolo <cosanopalmamanuel@gmail.com>
2024-03-07 22:55:50 +0100

    Funciones colaborador y jefe medio

--- Interface/IColaborador.java
+++ Interface/IColaborador.java
@@ -1,8 +1,12 @@
 package Interface;
 
+import Model.entity.EstadoTarea;
+import Model.entity.Proyecto;
+import Model.entity.Tarea;
+
 public interface IColaborador {
     //Metodos que se implementaran en la clase colaborador
-    void actualizarEstadoTarea();
-    void anadirComentario();
-    void editarComentario();
+    EstadoTarea actualizarEstadoTarea(String cambiarEstado, Tarea tarea);
+    String anadirComentario(Tarea tarea, Proyecto proyecto, String comentario);
+    //void editarComentario();
 }
